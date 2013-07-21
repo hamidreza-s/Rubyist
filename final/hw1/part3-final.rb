b14ffbc50811d0e95dadb12420197465a70efa9e
hamidrezas <hamidreza.s@gmail.com>
2013-07-21 11:40:00 -0700

    part 3 of hw 1 in progress

--- final/hw1/part3-final.rb
+++ final/hw1/part3-final.rb
@@ -2,11 +2,11 @@ def combine_anagrams(words)
 	result = []
 	mentioned = []
 	words.each do |x|
-		x_x = x.each_char.sort.join
+		x_x = x.downcase.each_char.sort.join
 		next if mentioned.include?(x_x)
 		temp = []
 		words.each do |y|
-			y_y = y.each_char.sort.join
+			y_y = y.downcase.each_char.sort.join
 			temp << y if x_x == y_y
 		end
 		result << temp
--- final/hw1/part3-inprogress.rb
+++ final/hw1/part3-inprogress.rb
@@ -2,11 +2,11 @@ def combine_anagrams(words)
 	result = []
 	mentioned = []
 	words.each do |x|
-		x_x = x.each_char.sort.join
+		x_x = x.downcase.each_char.sort.join
 		next if mentioned.include?(x_x)
 		temp = []
 		words.each do |y|
-			y_y = y.each_char.sort.join
+			y_y = y.downcase.each_char.sort.join
 			temp << y if x_x == y_y
 		end
 		result << temp
@@ -15,7 +15,7 @@ def combine_anagrams(words)
 	result
 end
 
-input = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
+input = ['Cars', 'For', 'Potatoes', 'Racs', 'Four', 'Scar', 'Creams', 'Scream']
 puts combine_anagrams(input).inspect
 
 # output: [ ["cars", "racs", "scar"],
